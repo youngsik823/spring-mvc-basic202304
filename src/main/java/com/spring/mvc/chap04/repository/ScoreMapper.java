6bbf0914a852219316ca9885d4e3f865dbc28270
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-04-27 16:20:30 +0900

    # 8. 마이바티스

--- src/main/java/com/spring/mvc/chap04/repository/ScoreMapper.java
+++ src/main/java/com/spring/mvc/chap04/repository/ScoreMapper.java
@@ -1,7 +1,20 @@
 package com.spring.mvc.chap04.repository;
 
+import com.spring.mvc.chap04.entity.Score;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ScoreMapper {
+    List<Score> findAll(String sort); // 정렬 목록조회
+
+    // 성적 정보 등록
+    boolean save(Score score);
+
+    // 성적 정보 한개 삭제
+    boolean deleteByStuNum(int stuNum);
+
+    // 성적 정보 개별 조회
+    Score findByStuNum(int stuNum);
 }
