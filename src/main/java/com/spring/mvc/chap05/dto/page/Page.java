549b5caff68e5c045ab302fffc5ef8ef1b662bf0
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-05-01 16:37:13 +0900

    # 9-3. 페이징 계산 알고리즘 setter 추가

--- src/main/java/com/spring/mvc/chap05/dto/page/Page.java
+++ src/main/java/com/spring/mvc/chap05/dto/page/Page.java
@@ -17,6 +17,22 @@ public class Page {
         this.amount = 6;
     }
 
+    public void setPageNo(int pageNo) {
+        if (pageNo < 1 || pageNo > Integer.MAX_VALUE) {
+            this.pageNo = 1;
+            return;
+        }
+        this.pageNo = pageNo;
+    }
+
+    public void setAmount(int amount) {
+        if (amount < 6 || amount > 10) {
+            this.amount = 6;
+            return;
+        }
+        this.amount = amount;
+    }
+
     public int getPageStart() {
         /*
             pageNo : 1 ->  return 0
