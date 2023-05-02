07e921477ff32c0bfbda9471359c3fcc83504a35
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-05-02 17:31:25 +0900

    # 11-1. 댓글 SQL 처리 및 테스트

--- src/test/java/com/spring/mvc/chap05/repository/ReplyMapperTest.java
+++ src/test/java/com/spring/mvc/chap05/repository/ReplyMapperTest.java
@@ -1,5 +1,6 @@
 package com.spring.mvc.chap05.repository;
 
+import com.spring.mvc.chap05.dto.page.Page;
 import com.spring.mvc.chap05.dto.page.Search;
 import com.spring.mvc.chap05.entity.Board;
 import com.spring.mvc.chap05.entity.Reply;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,31 +23,31 @@ class ReplyMapperTest {
     @Autowired
     ReplyMapper replyMapper;
 
-    @Test
-    @DisplayName("게시물 300개를 등록하고 " +
-            "각 게시물에 랜덤으로 1000개의 " +
-            "댓글을 나눠서 등록해야 한다.")
-    void bulkInsertTest() {
-
-        for (int i = 1; i <= 300; i++) {
-            Board b = Board.builder()
-                    .title("재밌는 게시물 " + i)
-                    .content("노잼 게시물 내용 " + i)
-                    .build();
-            boardMapper.save(b);
-        }
-        assertEquals(300, boardMapper.count(new Search()));
-
-        for (int i = 1; i <= 1000; i++) {
-            Reply r = Reply.builder()
-                    .replyWriter("잼민이 " + i)
-                    .replyText("말똥이~~~" + i)
-                    .boardNo((long) (Math.random() * 300 + 1))
-                    .build();
-            replyMapper.save(r);
-        }
-
-    }
+//    @Test
+//    @DisplayName("게시물 300개를 등록하고 " +
+//            "각 게시물에 랜덤으로 1000개의 " +
+//            "댓글을 나눠서 등록해야 한다.")
+//    void bulkInsertTest() {
+//
+//        for (int i = 1; i <= 300; i++) {
+//            Board b = Board.builder()
+//                    .title("재밌는 게시물 " + i)
+//                    .content("노잼 게시물 내용 " + i)
+//                    .build();
+//            boardMapper.save(b);
+//        }
+//        assertEquals(300, boardMapper.count(new Search()));
+//
+//        for (int i = 1; i <= 1000; i++) {
+//            Reply r = Reply.builder()
+//                    .replyWriter("잼민이 " + i)
+//                    .replyText("말똥이~~~" + i)
+//                    .boardNo((long) (Math.random() * 300 + 1))
+//                    .build();
+//            replyMapper.save(r);
+//        }
+//
+//    }
 
 
     @Test
@@ -85,6 +88,39 @@ class ReplyMapperTest {
         //then
         assertTrue(flag);
         assertEquals(3, replyMapper.count(boardNo));
+    }
+
+    @Test
+    @DisplayName("999번 댓글의 내용을 수정한 후 " +
+            "다시 조회했을 때 제목이 수정된 제목이어야 한다.")
+    @Transactional @Rollback
+    void modifyTest() {
+        //given
+        long replyNo = 999L;
+        String newReplyText = "수정댓그을";
+        Reply r = Reply.builder()
+                .replyText(newReplyText)
+                .replyNo(replyNo)
+                .build();
+        //when
+        boolean flag = replyMapper.modify(r);
+        //then
+        assertTrue(flag);
+        assertEquals(newReplyText, replyMapper.findOne(replyNo).getReplyText());
+    }
+
+    @Test
+    @DisplayName("3번 게시물의 댓글 목록을 조회했을 때" +
+            "리스트의 크기가 4이고, " +
+            "0번 인덱스의 댓글작성자가 잼민이 69여야 한다.")
+    void findAllTest() {
+        //given
+        long boardNo = 3L;
+        //when
+        List<Reply> replyList = replyMapper.findAll(boardNo, new Page());
+        //then
+        assertEquals(4, replyList.size());
+        assertEquals("잼민이 69", replyList.get(0).getReplyWriter());
     }
 
 }
