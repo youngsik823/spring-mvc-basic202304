f9739f8095ec2a20fe5906a764defc336870d790
youngsik823 <youngsik823@naver.com>
2023-05-07 11:32:41 +0900

    커밋

--- src/test/java/com/spring/mvc/chap05/repository/ReplyMapperTest.java
+++ src/test/java/com/spring/mvc/chap05/repository/ReplyMapperTest.java
@@ -23,31 +23,31 @@ class ReplyMapperTest {
     @Autowired
     ReplyMapper replyMapper;
 
-//    @Test
-//    @DisplayName("게시물 300개를 등록하고 " +
-//            "각 게시물에 랜덤으로 1000개의 " +
-//            "댓글을 나눠서 등록해야 한다.")
-//    void bulkInsertTest() {
-//
-//        for (int i = 1; i <= 300; i++) {
-//            Board b = Board.builder()
-//                    .title("재밌는 게시물 " + i)
-//                    .content("노잼 게시물 내용 " + i)
-//                    .build();
-//            boardMapper.save(b);
-//        }
-//        assertEquals(300, boardMapper.count(new Search()));
-//
-//        for (int i = 1; i <= 1000; i++) {
-//            Reply r = Reply.builder()
-//                    .replyWriter("잼민이 " + i)
-//                    .replyText("말똥이~~~" + i)
-//                    .boardNo((long) (Math.random() * 300 + 1))
-//                    .build();
-//            replyMapper.save(r);
-//        }
-//
-//    }
+    @Test
+    @DisplayName("게시물 300개를 등록하고 " +
+            "각 게시물에 랜덤으로 1000개의 " +
+            "댓글을 나눠서 등록해야 한다.")
+    void bulkInsertTest() {
+
+        for (int i = 1; i <= 300; i++) {
+            Board b = Board.builder()
+                    .title("재밌는 게시물 " + i)
+                    .content("노잼 게시물 내용 " + i)
+                    .build();
+            boardMapper.save(b);
+        }
+        assertEquals(300, boardMapper.count(new Search()));
+
+        for (int i = 1; i <= 1000; i++) {
+            Reply r = Reply.builder()
+                    .replyWriter("잼민이 " + i)
+                    .replyText("말똥이~~~" + i)
+                    .boardNo((long) (Math.random() * 300 + 1))
+                    .build();
+            replyMapper.save(r);
+        }
+
+    }
 
 
 //    @Test
